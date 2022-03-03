3bc3eb5029cf6c0c15a9fbde6be53a298c4396a5
Sheheryar Waseem <sherishaikh6@gmail.com>
2022-03-04 00:01:44 +0500

    final commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,91 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button, Form } from 'react-bootstrap';
+import { useEffect, useState } from "react"
+const axios = require('axios');
 
 function App() {
+
+
+  const [text, setText] = useState("");
+  const [messages, setMessages] = useState([]);
+
+
+  function sendMessage(e) {
+    e.preventDefault();
+
+    console.log("text: ", text);
+
+    setMessages((prev) => {
+      return [{ sender: "user", text: text }, ...prev];
+    });
+
+    axios.post(`https://sherifirtapp.herokuapp.com/talktochatbot`, {
+      text: text
+    })
+      .then((response) => {
+        console.log("response", response.data.text);
+
+        setMessages((prev) => {
+          return [{ sender: "bot", text: response.data.text }, ...prev];
+        });
+        e.target.reset();
+        setText("");
+
+      }).catch(error => {
+        console.log("error: ", error);
+
+        setMessages((prev) => {
+          return [{ sender: "bot", text: "dummy response from chatbot" }, ...prev,];
+        });
+        e.target.reset();
+        setText("");
+
+      })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <div className="container-fluid">
+        
+      <Form onSubmit={sendMessage}>
+        <Form.Group
+          style={{
+            display: "flex",
+            justifyContent: "space-between"
+          }} className="mb-3 mt-3" controlId="formBasicEmail">
+
+          <Form.Control
+            onChange={(e) => { setText(e.target.value) }}
+            type="text"
+            placeholder="Enter your message"
+          />
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form.Group>
+      </Form>
+
+      <br />
+      <br />
+      <br />
+
+      <div style={{ display: "flex", flexDirection: "column" }}>
+
+        {messages?.map((eachMessage, eachMessageIndex) => (
+          <div key={`${eachMessageIndex}-message`} style={{
+            display: "flex",
+            justifyContent: (eachMessage.sender === "user") ? "flex-end" : "flex-start"
+          }}>
+            <div className='border border-primary p-2'>{eachMessage.text}</div>
+          </div>
+        ))}
+
+      </div>
+      </div>
+      </div>
+      
+
   );
 }
 
